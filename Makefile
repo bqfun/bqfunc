bc575ef0859422d820215bc63eecb91a53fe0964
na0fu3y <17900178+na0fu3y@users.noreply.github.com>
2020-11-11 21:05:39 +0900

    add zetasql format

---
+++ Makefile
@@ -0,0 +1,2 @@
+fmt:
+	docker run --rm -v `pwd`:/home:Z matts966/zetasql-formatter:latest
--- udfs/zerobyte/_not_null_float64_to_ieee.sql
+++ udfs/zerobyte/_not_null_float64_to_ieee.sql
@@ -1,4 +1,2 @@
-CREATE OR REPLACE FUNCTION
-  zerobyte._NOT_NULL_FLOAT64_TO_IEEE(f FLOAT64)
-  RETURNS ARRAY<INT64>
-  LANGUAGE js AS "return Array.from(new Uint32Array(new Float64Array([f]).buffer))"
+CREATE OR REPLACE FUNCTION zerobyte._NOT_NULL_FLOAT64_TO_IEEE(f FLOAT64)
+RETURNS ARRAY< INT64 > LANGUAGE js AS "return Array.from(new Uint32Array(new Float64Array([f]).buffer))";
