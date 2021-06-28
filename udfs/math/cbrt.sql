8150d9ea84e861d54f45b229ee9cda78f8aa476d
Naofumi Yamada <17900178+na0fu3y@users.noreply.github.com>
2021-06-28 20:14:11 +0900

    make fmt

--- udfs/math/cbrt.sql
+++ udfs/math/cbrt.sql
@@ -1,6 +1,4 @@
-CREATE OR REPLACE FUNCTION
-  math.cbrt(x BIGNUMERIC)AS(
-  IF
-    (x < 0,
-      -POW(-x, 1/3),
-      POW(x, 1/3)));
+CREATE OR REPLACE FUNCTION math.cbrt(x BIGNUMERIC)
+AS (
+  `IF`(x < 0, - POW(- x, 1 / 3), POW(x, 1 / 3))
+);
